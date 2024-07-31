ee347265c4fcc77a1b7b5de27d20b6a7841d7dcc
ritika-abc <ritikasss321@gmail.com>
2024-07-31 13:26:51 +0530

    load

--- js/main.js
+++ js/main.js
@@ -77,6 +77,27 @@
             }
         }
     });
+    $(".caro").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+       
+        dots: true,
+        loop: true,
+        nav : false,
+        navText : [
+            '<i class="bi bi-chevron-left text-center "></i>',
+            '<i class="bi bi-chevron-right text-center "></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:3
+            }
+        }
+    });
 
 
     // Portfolio isotope and filter
